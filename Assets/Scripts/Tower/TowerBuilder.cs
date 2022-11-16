d1d8d75e641d76df1a4fb9c6c83103e434ccf79b
fantasstic <mrfantassticgame@gmail.com>
2022-11-16 19:16:46 +0200

    visual
    add colors

--- Assets/Scripts/Tower/TowerBuilder.cs
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -7,6 +7,7 @@ public class TowerBuilder : MonoBehaviour
     [SerializeField] private float _towerSize;
     [SerializeField] private Transform _buildPoint;
     [SerializeField] private Block _block;
+    [SerializeField] private Color[] _colors;
 
     private List<Block> _blocks;
 
@@ -19,6 +20,7 @@ public class TowerBuilder : MonoBehaviour
         for (int i = 0; i < _towerSize; i++)
         {
             Block newBlock = BuildBlock(currentPoint);
+            newBlock.SetColor(_colors[Random.Range(0, _colors.Length)]);
             _blocks.Add(newBlock);
             currentPoint = newBlock.transform;
         }
